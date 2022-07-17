111e1c492b4b56a6bf545bc325323d2173954b4d
Alec Bloss <abloss@libranext.com>
2022-07-16 21:22:33 -0500

    Initial commit of libtasn1 bundle

---
+++ base/libtasn1/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
+# Debian packaging code Copyright © 2022 Alec Bloss
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Extract the source archive
+tar xvzf libtasn1-4.18.0.tar.gz
+
+# Change directory to source
+cd libtasn1-4.18.0
+
+# Configure and compile package
+./configure --prefix=/usr --disable-static &&
+make
+
+# Install to package directory - NSPR has some weird auto/makefile stuff going on that DESTDIR doesn't quite behave as expected.
+## So normally you'd only have two levels of .., but for some reason it needs three. It works though.
+make DESTDIR=../package install
+
+# Build the debian package and rename it correctly.
+cd ../
+dpkg-deb --build package
+mv package.deb libtasn1_4.18.0_amd64.deb
