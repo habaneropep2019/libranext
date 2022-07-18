33040586950527618c741d583a6c5d1aa0f44b1e
Alec Bloss <abloss@libranext.com>
2022-07-17 19:42:32 -0500

    Initial commit of glibc

---
+++ base/glibc/build.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
+# Debian packaging code Copyright © 2022 Alec Bloss
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Extract the source archive
+tar xvf glibc-2.35.orig.tar.xz
+
+# Change directory to source
+cd glibc-2.35
+
+# Apply FHS patch
+patch -Np1 -i ../glibc-2.35-fhs-1.patch
+
+# Configure and compile package
+mkdir -v build && cd build
+echo "rootsbindir=/usr/sbin" > configparms
+../configure --prefix=/usr                            \
+             --disable-werror                         \
+             --enable-kernel=3.2                      \
+             --enable-stack-protector=strong          \
+             --with-headers=/usr/include              \
+             libc_cv_slibdir=/usr/lib
+make
+
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ../..
+workdir=`pwd`
+cd $workdir1
+
+touch $workdir/package/etc/ld.so.conf
+
+sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
+
+make DESTDIR=$workdir/package install
+
+cp -v ../nscd/nscd.conf $workdir/package/etc/nscd.conf
+
+mkdir -pv $workdir/package/var/cache/nscd
+
+mkdir -pv $workdir/package/usr/lib/locale
+
+cat > $workdir/package/etc/nsswitch.conf << "EOF"
+# Begin /etc/nsswitch.conf
+
+passwd: files
+group: files
+shadow: files
+
+hosts: files dns
+networks: files
+
+protocols: files
+services: files
+ethers: files
+rpc: files
+
+# End /etc/nsswitch.conf
+EOF
+
+cat > $workdir/package/etc/ld.so.conf << "EOF"
+# Begin /etc/ld.so.conf
+/usr/local/lib
+/opt/lib
+
+EOF
+
+cat >> $workdir/package/etc/ld.so.conf << "EOF"
+# Add an include directory
+include /etc/ld.so.conf.d/*.conf
+
+EOF
+mkdir -pv $workdir/package/etc/ld.so.conf.d
+
+# Remove info file so we can manage it with maintainer scripts
+
+cd $workdir/package/usr/share/info
+rm dir
+cd $workdir1
+
+# Build the debian package and rename it correctly.
+cd ../..
+dpkg-deb --build package
+mv package.deb glibc_2.35_amd64.deb
