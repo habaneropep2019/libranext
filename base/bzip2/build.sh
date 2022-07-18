9a7ee8fda39fbccf6f9ad68c5d6930d69b63f622
Alec Bloss <abloss@libranext.com>
2022-07-18 15:16:38 -0500

    Initial commit of bzip2

---
+++ base/bzip2/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
+# Debian packaging code Copyright © 2022 Alec Bloss
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Extract the source archive
+tar xvf bzip2-1.0.8.orig.tar.gz
+
+# Change directory to source
+cd bzip2-1.0.8
+
+# Patch code and various fixes
+patch -Np1 -i ../bzip2-1.0.8-install_docs-1.patch
+sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
+sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
+make -f Makefile-libbz2_so
+make clean
+
+# Configure and compile package
+make
+
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ..
+workdir=`pwd`
+cd $workdir1
+make PREFIX=$workdir/package/usr install
+
+# Build the debian package and rename it correctly.
+cd ../
+dpkg-deb --build package
+mv package.deb bzip2_1.0.8_amd64.deb
