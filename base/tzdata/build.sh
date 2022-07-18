314a46136406680dd0353a5e11fc1a6306747ff5
Alec Bloss <abloss@libranext.com>
2022-07-18 13:07:38 -0500

    Initial commit of tzdata

---
+++ base/tzdata/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
+# Debian packaging code Copyright © 2022 Alec Bloss
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Extract the source archive
+mkdir tzdata2021e
+cd tzdata2021e
+tar xvf ../tzdata2021e.orig.tar.gz
+
+
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ..
+workdir=`pwd`
+cd $workdir1
+ZONEINFO=$workdir/package/usr/share/zoneinfo
+mkdir -pv $ZONEINFO/{posix,right}
+
+for tz in etcetera southamerica northamerica europe africa antarctica  \
+          asia australasia backward; do
+    zic -L /dev/null   -d $ZONEINFO       ${tz}
+    zic -L /dev/null   -d $ZONEINFO/posix ${tz}
+    zic -L leapseconds -d $ZONEINFO/right ${tz}
+done
+
+cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
+zic -d $ZONEINFO -p America/New_York
+unset ZONEINFO
+
+# Build the debian package and rename it correctly.
+cd $workdir
+dpkg-deb --build package
+mv package.deb tzdata_2021e_amd64.deb
