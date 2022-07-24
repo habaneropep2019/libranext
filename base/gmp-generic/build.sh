6873319d6401d8bafd8a438a371486ee5684f1ee
Alec Bloss <abloss@libranext.com>
2022-07-24 15:13:59 -0500

    Initial commit of gmp (gmp-generic)

---
+++ base/gmp-generic/build.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
+# Debian packaging code Copyright © 2022 Alec Bloss
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Extract the source archive
+tar xvf gmp-6.2.1.orig.tar.xz
+
+# Change directory to source
+cd gmp-6.2.1
+
+# Configure and compile package
+## Make sure we configure for a generic processor
+cp -v configfsf.guess config.guess
+cp -v configfsf.sub   config.sub
+
+./configure --prefix=/usr    \
+            --enable-cxx     \
+            --disable-static \
+            --build=x86_64-pc-linux-gnu \
+            --docdir=/usr/share/doc/gmp-6.2.1
+make
+make html
+
+# GMP should have the testsuite run, at least one time per change in version or buildscript to ensure it's working correctly.
+## By default, the testsuite does not run to allow automated builds. If buildling this manually, run the testsuite by passing
+## -t as a command line option to this build script.
+
+function testsuite {
+	make check 2>&1 | tee gmp-check-log
+	
+	echo ""
+	echo ""
+	echo "Test results"
+	echo "=============="
+	awk '/# PASS:/{total+=$3} ; END{print total}' gmp-check-log
+	sleep 2s
+	echo "There should be 197 tests passed."
+	read -p 'Do you accept the test results? (yes/no):' testaccept
+
+	if [ $testaccept == "no" ]
+	then
+		exit 1
+	fi
+}
+
+while getopts "t" option; do
+case ${option} in
+t )
+echo "Running test suite..."
+testsuite
+;;
+\? )
+echo "Not running test suite..."
+sleep 2s
+;;
+esac
+done
+
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ..
+workdir=`pwd`
+cd $workdir1
+make DESTDIR=$workdir/package install
+make DESTDIR=$workdir/package install-html
+
+# Remove info file so we can manage it with maintainer scripts
+
+cd $workdir/package/usr/share/info
+rm dir
+cd $workdir1
+
+# Build the debian package and rename it correctly.
+cd ../
+dpkg-deb --build package
+mv package.deb gmp-generic_6.2.1_amd64.deb
