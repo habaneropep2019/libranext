e9cf81fa0d1d27cc4802f5faab4aab3638cd3d0c
Alec Bloss <abloss@libranext.com>
2022-07-28 19:01:59 -0500

    Initial commit of syntax-keyword-try

---
+++ perl-modules/Syntax-Keyword-Try/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
+# Debian packaging code Copyright © 2022 Alec Bloss
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Extract the source archive
+tar xvf Syntax-Keyword-Try-0.27.orig.tar.gz
+
+# Change directory to source
+cd Syntax-Keyword-Try-0.27
+
+# Configure and compile package
+perl Build.PL
+./Build
+
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ..
+workdir=`pwd`
+cd $workdir1
+./Build install --destdir=$workdir/package
+
+# Remove unecessary pod file to avoid conflicts
+if [ -e $workdir/package/usr/lib/perl5/5.34/core_perl/perllocal.pod ]
+then
+	rm -v $workdir/package/usr/lib/perl5/5.34/core_perl/perllocal.pod
+fi
+
+# Build the debian package and rename it correctly.
+cd ../
+dpkg-deb --build package
+mv package.deb syntax-keyword-try_0.27_amd64.deb
