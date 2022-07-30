6c417b444f7445d9ce01b19a9440c24ddf7d0a70
Alec Bloss <abloss@libranext.com>
2022-07-29 19:13:33 -0500

    Initial commit of libassuan

---
+++ enhanced-base/libassuan/build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
+# Debian packaging code Copyright © 2022 Alec Bloss
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# Extract the source archive
+tar xvfj libassuan-2.5.5.orig.tar.bz2
+
+# Change directory to source
+cd libassuan-2.5.5
+
+# Configure and compile package
+./configure --prefix=/usr &&
+make                      &&
+
+make -C doc html                                                       &&
+makeinfo --html --no-split -o doc/assuan_nochunks.html doc/assuan.texi &&
+makeinfo --plaintext       -o doc/assuan.txt           doc/assuan.texi
+
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ..
+workdir=`pwd`
+cd $workdir1
+make DESTDIR=$workdir/package install
+
+install -v -dm755   $workdir/package/usr/share/doc/libassuan-2.5.5/html &&
+install -v -m644 doc/assuan.html/* \
+                    $workdir/package/usr/share/doc/libassuan-2.5.5/html &&
+install -v -m644 doc/assuan_nochunks.html \
+                    $workdir/package/usr/share/doc/libassuan-2.5.5      &&
+install -v -m644 doc/assuan.{txt,texi} \
+                    $workdir/package/usr/share/doc/libassuan-2.5.5
+
+# Remove info file so we can manage it with maintainer scripts
+
+cd $workdir/package/usr/share/info
+rm dir
+cd $workdir1
+
+# Build the debian package and rename it correctly.
+cd ../
+dpkg-deb --build package
+mv package.deb libassuan_2.5.5_amd64.deb
