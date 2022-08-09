d82ce82c69f746d127d78cac26132bdbc528a946
Alec Bloss <abloss@libranext.com>
2022-08-08 21:52:01 -0500

    RELEASE grep_3.7-1

--- base/grep/releases/3.7-1/build.sh
+++ base/grep/releases/3.7-1/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
 # Debian packaging code Copyright © 2022 Alec Bloss
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -8,43 +14,33 @@
 #
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-function build-37 {
-	pushd releases/3.7/
-	bash build.sh
-	popd
-	cp releases/3.7/*.deb .
-}
+# Extract the source archive
+tar xvf grep-3.7.orig.tar.xz
 
-function build-37-1 {
-	pushd releases/3.7-1/
-	bash build.sh
-	popd
-	cp releases/3.7-1/*.deb .
-}
+# Change directory to source
+cd grep-3.7
 
-function latest {
-	build-37-1
-}
+# Configure and compile package
+./configure --prefix=/usr
 
+make
 
-if [ -z "$1" ]
-then
-	echo "Defaulting to latest version..."
-	latest
-else
-	case "$1" in
-		3.7)
-			build-37
-			;;
-			3.7-1)
-			build-37-1
-			;;
-		*)
-			echo 'Fatal: Invalid option'
-			echo 'Available versions:'
-			echo '- 3.7-1 (default)'
-			echo '- 3.7'
-			exit 1
-			;;
-		esac
-fi
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ..
+workdir=`pwd`
+cd $workdir1
+make DESTDIR=$workdir/package install
+
+# Remove info file so we can manage it with maintainer scripts
+
+cd $workdir/package/usr/share/info
+rm dir
+cd $workdir1
+
+# Build the debian package and rename it correctly.
+cd ../
+dpkg-deb --build package
+mv package.deb grep_3.7-1_amd64.deb
