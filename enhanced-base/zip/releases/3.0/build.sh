0e4228eea6c3e7113e865e972c1e7237e7c7ae78
Alec Bloss <abloss@libranext.com>
2022-08-08 21:29:11 -0500

    Add multiversion build support

--- enhanced-base/zip/releases/3.0/build.sh
+++ enhanced-base/zip/releases/3.0/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Instructions extracted from the Linux from Scratch book:
+# Copyright © 1999-2022 Gerard Beekmans
+#
+# Instructions extracted from the Beyond Linux from Scratch Book:
+# Copyright © 1999-2022 The BLFS Development Team
+#
 # Debian packaging code Copyright © 2022 Alec Bloss
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -8,43 +14,25 @@
 #
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-function build-30 {
-	pushd releases/30/
-	bash build.sh
-	popd
-	cp releases/3.0/*.deb .
-}
+# Extract the source archive
+tar xvf zip30.orig.tar.gz
 
-function build-30-deb12 {
-	pushd releases/3.0-deb12/
-	bash build.sh
-	popd
-	cp releases/3.0-deb12/*.deb .
-}
+# Change directory to source
+cd zip30
 
-function latest {
-	build-30-deb12
-}
+# Configure and compile package.
+make -f unix/Makefile generic_gcc
 
+# Install to package directory
+unset workdir
+unset workdir1
+workdir1=`pwd`
+cd ..
+workdir=`pwd`
+cd $workdir1
+make prefix=$workdir/package/usr MANDIR=$workdir/package/usr/share/man/man1 -f unix/Makefile install
 
-if [ -z "$1" ]
-then
-	echo "Defaulting to latest version..."
-	latest
-else
-	case "$1" in
-		3.0-deb12)
-			build-30-deb12
-			;;
-		3.0)
-			build-30
-			;;
-		*)
-			echo 'Fatal: Invalid option'
-			echo 'Available versions:'
-			echo '- 3.0-deb12 (default)'
-			echo '- 3.0'
-			exit 1
-			;;
-		esac
-fi
+# Build the debian package and rename it correctly.
+cd ../
+dpkg-deb --build package
+mv package.deb zip_3.0_amd64.deb
